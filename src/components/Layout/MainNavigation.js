08a313016cb97776f8d87fa8eb5b47a2a5f9f780
Man Ping Chen <manpingchen@gmail.com>
2022-03-04 16:23:55 +0000

    Display navigation dynamically

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,24 +1,34 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import classes from './MainNavigation.module.css';
+import classes from "./MainNavigation.module.css";
+import AuthContext from "../../store/auth-context";
+import { Fragment, useContext } from "react";
 
 const MainNavigation = () => {
+  const { isLoddgedIn } = useContext(AuthContext);
+
   return (
     <header className={classes.header}>
-      <Link to='/'>
+      <Link to="/">
         <div className={classes.logo}>React Auth</div>
       </Link>
       <nav>
         <ul>
-          <li>
-            <Link to='/auth'>Login</Link>
-          </li>
-          <li>
-            <Link to='/profile'>Profile</Link>
-          </li>
-          {/* <li>
-            <button>Logout</button>
-          </li> */}
+          {!isLoddgedIn && (
+            <li>
+              <Link to="/auth">Login</Link>
+            </li>
+          )}
+          {isLoddgedIn && (
+            <Fragment>
+              <li>
+                <Link to="/profile">Profile</Link>
+              </li>
+              <li>
+                <button>Logout</button>
+              </li>
+            </Fragment>
+          )}
         </ul>
       </nav>
     </header>
